089d0f51e07e4c8d8809e6c714ed34e3ffe52913
silberbullet <pkw961028@naver.com>
2025-03-12 19:58:34 +0900

    feat(ironman):add body main

--- module-body/src/main/java/Body.java
+++ module-body/src/main/java/Body.java
@@ -1,6 +1,19 @@
+import Port.LeftArmPort;
+
+import java.util.ServiceLoader;
+
 public class Body {
+    public static void main(String[] args) {
+        // 모듈 등록
+        Core core = new Core();
+        RightArmV1 rightArm = new RightArmV1();
+        LeftArmPort leftArm = ServiceLoader.load(LeftArmPort.class)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No LeftArm implementation found"));
 
-
+        // 아이언맨 실행
         core.activate();
+        rightArm.activate();
+        leftArm.activate();
     }
 }
